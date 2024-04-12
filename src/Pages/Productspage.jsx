df3e537e1de2c97304382e75f32fa6809d40d3aa
Mhando-Z <Mhandosz17@gmail.com>
2024-04-12 22:25:40 +0300

    Phase One

--- src/Pages/Productspage.jsx
+++ src/Pages/Productspage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import ProductsContext from "../Context/ProductsContext";
@@ -8,6 +8,13 @@ import SimilarProducts from "../Components/SimilarProducts";
 function Productspage() {
   const { data } = useContext(ProductsContext);
   const { handleAddtoCart } = useContext(CartContext);
+  const [showContent, setContent] = useState(false);
+  const [showContent1, setContent1] = useState(true);
+
+  const handleContentView = () => {
+    setContent(!showContent);
+    setContent1(!showContent1);
+  };
 
   const { id } = useParams();
   if (!data || data.length === 0) {
@@ -29,34 +36,52 @@ function Productspage() {
     <div className="flex flex-col justify-center">
       <div className="flex items-center justify-center">
         <div className="flex flex-col lg:flex-row gap-y-5 gap-x-14 items-center justify-between p-10 bg-gray-300 shadow-xl rounded-xl">
-          {dataz.sku === null ? (
-            <img
-              src={dataz.image.url}
-              alt="phone"
-              className="max-w-screen h-auto bg-gray-200 shadow-md rounded-xl"
-            />
-          ) : (
-            <div>
-              <iframe
-                title={dataz.name}
-                width="500"
-                height="500"
-                src={`https://embed.studio.binkies3d.com/live3d/${dataz.sku}`}
-                frameborder="0"
-                allowfullscreen
-                className="sm:hidden size-80 shadow-md rounded-xl"
-              ></iframe>
-              <iframe
-                title={dataz.name}
-                width="500"
-                height="500"
-                src={`https://embed.studio.binkies3d.com/live3d/${dataz.sku}`}
-                frameborder="0"
-                allowfullscreen
-                className="hidden sm:flex shadow-md rounded-xl"
-              ></iframe>
-            </div>
-          )}
+          <div className="flex flex-col items-center justify-center">
+            {showContent1 && (
+              <div className="flex flex-col items-center justify-center gap-y-4">
+                <img
+                  src={dataz.image.url}
+                  alt="phone"
+                  className="max-w-screen h-auto bg-gray-200 shadow-md rounded-xl"
+                />
+
+                <Link
+                  onClick={handleContentView}
+                  className="py-2 px-8 bg-pink-500 text-white font-semibold rounded-md"
+                >
+                  3D model
+                </Link>
+              </div>
+            )}
+            {showContent && (
+              <div className="flex flex-col items-center justify-center gap-y-4">
+                <iframe
+                  title={dataz.name}
+                  width="500"
+                  height="500"
+                  src={`https://embed.studio.binkies3d.com/live3d/${dataz.sku}`}
+                  frameborder="0"
+                  allowfullscreen
+                  className="sm:hidden size-80 shadow-lg rounded-xl"
+                ></iframe>
+                <iframe
+                  title={dataz.name}
+                  width="500"
+                  height="500"
+                  src={`https://embed.studio.binkies3d.com/live3d/${dataz.sku}`}
+                  frameborder="0"
+                  allowfullscreen
+                  className="hidden sm:flex shadow-lg rounded-xl"
+                ></iframe>
+                <Link
+                  onClick={handleContentView}
+                  className="py-2 px-8 bg-pink-500 text-white font-semibold rounded-md"
+                >
+                  Picture
+                </Link>
+              </div>
+            )}
+          </div>
           <div className="flex flex-col items-center justify-center gap-1">
             <div className="mb-5">
               <h1 className="lg:text-6xl max-w-lg text-4xl mb-5 text-center">
@@ -91,14 +116,14 @@ function Productspage() {
               <div className="lg:hidden flex flex-col mt-3 gap-y-5 items-center justify-center">
                 <Link
                   onClick={scrollToTop}
-                  className="text-md w-full py-2 font-semibold px-10 bg-pink-600 text-white text-center"
+                  className="text-md w-full py-2 font-semibold px-10 bg-pink-500 text-white text-center"
                   to={`/Paypage/${dataz.id}`}
                 >
                   Buy
                 </Link>
                 <button
                   onClick={() => handleAddtoCart(dataz.id, 1)}
-                  className="text-md w-full py-2 items-center font-semibold px-10 bg-pink-600 text-white text-center"
+                  className="text-md w-full py-2 items-center font-semibold px-10 bg-pink-500 text-white text-center"
                 >
                   <ShoppingCartIcon sx={{ fontSize: "1.2rem", mr: "7px" }} />
                   Add Cart
