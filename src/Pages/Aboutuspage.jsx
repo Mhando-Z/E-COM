cc05784687d1af2d23362a03fba7e3c93b4f312d
Mhando-Z <Mhandosz17@gmail.com>
2024-05-17 17:51:39 +0300

    Phase Two

--- src/Pages/Aboutuspage.jsx
+++ src/Pages/Aboutuspage.jsx
@@ -9,10 +9,10 @@ function Aboutuspage() {
         </h1>
         <div className="flex flex-col gap-y-5 justify-between">
           <div className="p-5  dark:bg-gray-700 bg-slate-300 shadow-md rounded-xl">
-            <h1 className="md:text-3xl dark:text-black mb-2 text-2xl font-semibold uppercase bg-gradient-to-r from-gray-400 p-2 via-transparent to-transparent">
+            <h1 className="md:text-3xl dark:text-black mb-2 text-xl font-semibold uppercase bg-gradient-to-r from-gray-400 p-2 via-transparent to-transparent">
               Introduction
             </h1>
-            <h2 className="text-2xl text-justify md:tracking-normal tracking-tighter">
+            <h2 className="lg:text-2xl md:text-xl  text-justify md:tracking-normal tracking-tighter">
               Welcome to E-COM, your one-stop online shop for affordable goodies
               straight from China! At E-COM, we believe that quality products
               shouldn't break the bank. That's why we've scoured China to bring
@@ -21,10 +21,10 @@ function Aboutuspage() {
             </h2>
           </div>
           <div className="p-5  dark:bg-gray-700 bg-slate-300 shadow-md rounded-xl">
-            <h1 className="md:text-3xl dark:text-black text-2xl mb-2 uppercase font-semibold bg-gradient-to-r from-gray-400 p-2 via-transparent to-transparent">
+            <h1 className="md:text-3xl dark:text-black text-xl mb-2 uppercase font-semibold bg-gradient-to-r from-gray-400 p-2 via-transparent to-transparent">
               Mission
             </h1>
-            <h2 className="text-2xl text-justify md:tracking-normal tracking-tighter ">
+            <h2 className="lg:text-2xl md:text-xl text-justify md:tracking-normal tracking-tighter ">
               Our mission is simple: to make shopping for the things you love
               easy and accessible. Whether you're after trendy fashion finds,
               cutting-edge gadgets, or practical everyday essentials, we've got
@@ -35,7 +35,7 @@ function Aboutuspage() {
             </h2>
           </div>
           <div className="p-5  dark:bg-gray-700 bg-slate-300 shadow-md rounded-xl ">
-            <h2 className="text-2xl text-justify md:tracking-normal tracking-tighter">
+            <h2 className="lg:text-2xl md:text-xl text-justify md:tracking-normal tracking-tighter">
               Our user-friendly website makes browsing and ordering a breeze, so
               you can spend less time shopping and more time enjoying your new
               purchases. Join the Naima family today and experience the joy of
